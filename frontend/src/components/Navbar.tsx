7a81a0c7e3e78740fb48657449f3ee9073f75b35
segevGr <segev126@gmail.com>
2025-06-13 13:22:01 +0300

    upgrade navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { Music, User, LogOut } from "lucide-react";
+import { Music, User, LogOut, Users } from "lucide-react";
 import { logout } from "../store/authSlice";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
-import { useAppDispatch } from "../store/storeHooks";
+import { useAppDispatch, useAppSelector } from "../store/storeHooks";
 import ModalDialog from "./ModalDialog";
 
 const Navbar = () => {
@@ -11,11 +11,32 @@ const Navbar = () => {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-
+  const { role } = useAppSelector((state) => state.auth);
   const confirmLogout = () => {
     dispatch(logout());
     navigate(ROUTES.HOME);
   };
+
+  const listButtons = [
+    {
+      label: "my profile",
+      onClick: () => {},
+      icon: <User />,
+      role: ["user", "admin"],
+    },
+    {
+      label: "manage users",
+      onClick: () => {},
+      icon: <Users />,
+      role: ["admin"],
+    },
+    {
+      label: "Logout",
+      onClick: () => setShowLogoutModal(true),
+      icon: <LogOut />,
+      role: ["user", "admin"],
+    },
+  ];
 
   return (
     <header className="bg-primary text-textOnDark flex items-center justify-between px-6 py-4 shadow-md relative">
@@ -33,14 +54,28 @@ const Navbar = () => {
         </button>
 
         {open && (
-          <div className="absolute right-0 mt-2 w-32 bg-white rounded-md shadow-lg z-50">
-            <button
-              onClick={() => setShowLogoutModal(true)}
-              className="w-full flex items-center gap-2 px-4 py-2 text-sm text-errorText hover:opacity-50"
-            >
-              <LogOut size={16} />
-              Logout
-            </button>
+          <div className="absolute right-0 mt-2 w-44 bg-white rounded-md shadow-lg z-50">
+            {listButtons
+              .filter((button) => button.role.includes(role || ""))
+              .map((button, index, array) => (
+                <div key={button.label}>
+                  <button
+                    onClick={button.onClick}
+                    className={`w-full flex items-center gap-2 px-4 py-2 text-md font-medium hover:opacity-50 ${
+                      button.label === "Logout"
+                        ? "text-errorText"
+                        : "text-primary"
+                    }`}
+                  >
+                    {button.icon}
+                    {button.label}
+                  </button>
+
+                  {index < array.length - 1 && (
+                    <div className="border-t border-gray-200 my-1" />
+                  )}
+                </div>
+              ))}
           </div>
         )}
       </div>
