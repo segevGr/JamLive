cc38f63aae4d652d432f29165a64d6ecfa492178
segevGr <segev126@gmail.com>
2025-07-18 01:03:01 +0300

    fix: fix routes export

--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -1,4 +1,4 @@
-export default {
+const ROUTS = {
   HOME: "/",
   LOGIN: "/login",
   REGISTER: "/register",
@@ -9,3 +9,5 @@ export default {
   JAM: "/jam",
   PROFILE: "/profile",
 };
+
+export default ROUTS;
