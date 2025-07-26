e770387d20b2bf9fa45968cdac5c7403689e0c36
segevGr <segev126@gmail.com>
2025-07-26 23:05:20 +0300

    refactor(i18n): default to English and remove auto language detection

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./enTranslation.json";
 import he from "./heTranslation.json";
 
@@ -10,19 +9,14 @@ export const resources = {
   he: { translation: he },
 } as const;
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: "en",
-    ns: [defaultNS],
-    debug: false,
-    interpolation: { escapeValue: false },
-    detection: {
-      order: ["localStorage", "navigator"],
-      caches: ["localStorage"],
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: localStorage.getItem("i18nextLng") || "en",
+  fallbackLng: "en",
+  load: "languageOnly",
+  ns: [defaultNS],
+  debug: false,
+  interpolation: { escapeValue: false },
+});
 
 export default i18n;
